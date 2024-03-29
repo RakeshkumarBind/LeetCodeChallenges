e2ca9a294986037bf853d220d968b5e647f86dfc
BIND RAKESHKUMAR PRABHURAM <109387080+RakeshkumarBind@users.noreply.github.com>
2024-03-29 22:53:33 +0530

    Update firstsql.py

--- SQL/firstsql.py
+++ SQL/firstsql.py
@@ -1,1 +1,8 @@
+# JOIN Operation
+# INNER JOIN ---  only the common from both must be displayed 
+
+# select column from table1 inner join table2 on table1.columnname = table2.columnname
+
+# LEFT JOIN ------------> ALL left table element must be counted if not possible than add null value for those element which are not available in right table
+# Same rught join
 
